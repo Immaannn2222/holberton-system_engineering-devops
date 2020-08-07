e7b413ae024842275829128efca4c329e7d229a6
Immaannn2222 <imennaayari@gmail.com>
2020-08-07 20:16:24 +0100

    f

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Connect to a server without typing a password(Puppet)
 file_line {'Turn off passwd auth':
-ensure => 'present'
-path   => '/etc/ssh/ssh_config',
-line   => 'PasswordAuthentication no',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
-file_line {'Declare identity file':
-ensure => 'present',
-path   => '/etc/ssh/ssh_config',
-line   => 'IdentityFile ~/.ssh/holberton',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
