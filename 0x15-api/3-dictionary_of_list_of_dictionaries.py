ad33468dcb3ce59e875da3b21aa53fd03584be9e
Immaannn2222 <imennaayari@gmail.com>
2020-09-14 18:13:44 +0100

    g

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,14 +12,16 @@ if __name__ == "__main__":
     user = requests.get('https://jsonplaceholder.typicode.com/users')
     list_json = to_do.json()
     user_json = user.json()
-    for i in user_json:
-        user_name = i.get("username")
-        i_d = i.get('id')
-    usr = {i_d: []}
+    usr = {}
     with open('todo_all_employees' + '.json', mode='w') as f:
-        for t in list_json:
-            Dict = {}
-            Dict = {"task": t.get("title"), "completed": t.get("completed"),
-                    "username": user_name}
-            usr[i_d].append(Dict)
+        for i in user_json:
+            user_name = i.get("username")
+            i_d = i.get('id')
+            usr[i_d] = []
+            for t in list_json:
+                if i_d == t['userId']:
+                    Dict = {}
+                    Dict = {"username": user_name, "task": t.get("title"),
+                            "completed": t.get("completed")}
+                    usr[i_d].append(Dict)
         json.dump(usr, f)
