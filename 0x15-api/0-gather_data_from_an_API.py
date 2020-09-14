d792336bdeb502767bf0b7b37298b86fb695020d
Immaannn2222 <imennaayari@gmail.com>
2020-09-14 16:08:48 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""HTTP WITH PYTHON"""
+from sys import argv
+import csv
+import requests
+
+
+if __name__ == "__main__":
+    """main"""
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                         params={"userId": argv[1]})
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
+                        params={"id": argv[1]})
+    list_json = to_do.json()
+    user_json = user.json()
+
+    for i in user_json:
+        name = i.get("name")
+    compl_tasks = 0
+    for j in list_json:
+        if j.get("completed"):
+            compl_tasks += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, compl_tasks,
+          len(list_json)))
+    for x in list_json:
+        if x.get("completed"):
+            print("\t" + " " + x.get("title"))
